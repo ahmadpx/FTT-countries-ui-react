b7535e6d2a24337cd3c56e18bb340ffb88ebce29
Ahmed-osama <ahmed.osama@tajawal.com>
2019-09-25 13:33:48 +0400

    frame wor padge

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React, { useEffect } from 'react';
 import CountriesListContainer from './containers/CountriesListContainer';
 import FiltersContainer from './containers/FiltersContainer';
-import {
-  Layout,
-  GlobalStyle
-} from './App.styles'
-
-
+import { Layout, GlobalStyle, FrameworkPadge } from './App.styles';
+import reactIcon from './logo.svg';
 function App({ store }) {
   useEffect(() => {
     store.countriesStore.fetch(function onError(error) {
       console.error(error);
     });
-  }, []);
+  }, [store.countriesStore]);
 
   return (
     <>
-
+      <FrameworkPadge>
+        <img src={reactIcon} />
+      </FrameworkPadge>
       <GlobalStyle />
       <Layout className="App">
         <FiltersContainer store={store} />
